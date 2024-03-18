fc84880c24c1f45e71e0542393dcfa51ebd244cb
aHmEdaBoElSoOd33 <20912018100378@fci.zu.edu.eg>
2024-03-18 10:50:34 +0200

    Initial Commit

---
+++ CustomSideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSideMenu
+//
+//  Created by 2B on 18/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
